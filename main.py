998fdcb13a9b4fe5a08ca833522b69dde68957d4
Roger Vinas <roger.vinas@n26.com>
2025-08-14 00:21:49 +0300

    Rotate

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.rotate(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
